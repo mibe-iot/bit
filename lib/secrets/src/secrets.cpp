e46b1ddb241859ca991d02d1ad40489ef943569f
Maksim Yersh <edelwud@icloud.com>
2022-01-23 02:27:59 +0300

    init: secrets module

--- lib/secrets/src/secrets.cpp
+++ lib/secrets/src/secrets.cpp
@@ -1,14 +1,40 @@
 #include <secrets.h>
-#include <ArduinoJson.h>
 
 extern const uint8_t secrets_json_start[] asm("_binary_secrets_json_start");
 extern const uint8_t secrets_json_end[] asm("_binary_secrets_json_end");
 
 DynamicJsonDocument doc(1024);
 
+JsonObject Secrets::GetJsonObject()
+{
+    deserializeJson(doc, secrets_json_start);
+    return doc.as<JsonObject>();
+}
+
 std::string Secrets::GetBLEServiceName()
 {
     deserializeJson(doc, secrets_json_start);
     JsonObject obj = doc.as<JsonObject>();
     return obj[F("SERVICE_NAME")];
+}
+
+std::string Secrets::GetBLEServiceUUID()
+{
+    deserializeJson(doc, secrets_json_start);
+    JsonObject obj = doc.as<JsonObject>();
+    return obj[F("SERVICE_UUID")];
+}
+
+std::string Secrets::GetWiFiSSID()
+{
+    deserializeJson(doc, secrets_json_start);
+    JsonObject obj = doc.as<JsonObject>();
+    return obj[F("WIFI_SSID")];
+}
+
+std::string Secrets::GetWiFiPassword()
+{
+    deserializeJson(doc, secrets_json_start);
+    JsonObject obj = doc.as<JsonObject>();
+    return obj[F("WIFI_PASSWORD")];
 }
