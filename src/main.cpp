7a840bf2774bc75c637343f6c6edcbb683b17880
Maksim Yersh <edelwud@icloud.com>
2022-01-23 16:47:44 +0300

    integrate: connection notifier

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,33 @@
 #include <WiFiWorker.h>
 #include <BLEWorker.h>
 
+#define LED 5
+
+[[noreturn]] void NotifierWorker(void *param) {
+    auto flags = (EventGroupHandle_t)param;
+
+    while (true) {
+        digitalWrite(LED, LOW);
+        xEventGroupWaitBits(flags, WifiConnected | BLEConnected, pdFALSE, pdTRUE, portMAX_DELAY);
+        digitalWrite(LED, HIGH);
+        vTaskDelay(pdMS_TO_TICKS(300));
+    }
+}
+
 void setup() {
-  EventGroupHandle_t flags = xEventGroupCreate();
+    EventGroupHandle_t flags = xEventGroupCreate();
 
-  Serial.begin(115200);
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
+    Serial.begin(115200);
 
-  xTaskCreate(BLEWorker::TaskHandler, "BLEWorker::TaskHandler", 8192, flags, 1, nullptr);
-  xTaskCreate(WiFiWorker::TaskHandler, "WiFiWorker::TaskHandler", 8192, flags, 1, nullptr);
+    xTaskCreate(BLEWorker::TaskHandler, "BLEWorker::TaskHandler", 8192, flags, 1, nullptr);
+    xTaskCreate(WiFiWorker::TaskHandler, "WiFiWorker::TaskHandler", 8192, flags, 1, nullptr);
+    xTaskCreate(NotifierWorker, "Notifier", 1024, flags, 10, nullptr);
 
-  Serial.println("Waiting a client connection to notify...");
+    Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  vTaskDelete(nullptr);
+    vTaskDelete(nullptr);
 }
