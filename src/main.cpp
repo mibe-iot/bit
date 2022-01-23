c0493f3d23df3bb7ed763d00461d20e23b0e90a6
Maksim Yersh <edelwud@icloud.com>
2022-01-23 15:08:49 +0300

    integrate: ble extended module

--- src/main.cpp
+++ src/main.cpp
@@ -1,96 +1,19 @@
 #include <Arduino.h>
-#include <BLEDevice.h>
-#include <BLEServer.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
 #include <secrets.h>
 #include <WiFiWorker.h>
+#include <BLEWorker.h>
 
-BLEServer *pServer = NULL;
-BLECharacteristic *pTxCharacteristic;
-bool deviceConnected = false;
-bool oldDeviceConnected = false;
-uint8_t txValue = 0;
-
-#define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
-#define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
-
-class BitBLEServer : public BLEServerCallbacks
-{
-  void onConnect(BLEServer *pServer)
-  {
-    deviceConnected = true;
-  };
-
-  void onDisconnect(BLEServer *pServer)
-  {
-    deviceConnected = false;
-  }
-};
-
-class BitBLECharacteristic : public BLECharacteristicCallbacks
-{
-  void onWrite(BLECharacteristic *pCharacteristic)
-  {
-    std::string rxValue = pCharacteristic->getValue();
-
-    if (rxValue.length() > 0)
-    {
-      Serial.println("*********");
-      Serial.print("Received Value: ");
-      for (int i = 0; i < rxValue.length(); i++)
-        Serial.print(rxValue[i]);
-
-      Serial.println();
-      Serial.println("*********");
-    }
-  }
-};
-
-[[noreturn]] void notifierTask(void *param)
-{
-  while (true)
-  {
-    pTxCharacteristic->setValue(&txValue, 1);
-    pTxCharacteristic->notify();
-    txValue++;
-    vTaskDelay(100 / portTICK_PERIOD_MS);
-  }
-}
-
-void setup()
-{
+void setup() {
   EventGroupHandle_t flags = xEventGroupCreate();
 
   Serial.begin(115200);
 
-  BLEDevice::init(Secrets::GetBLEServiceName());
-
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new BitBLEServer());
-
-  BLEService *pService = pServer->createService(Secrets::GetBLEServiceUUID());
-
-  pTxCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID_TX,
-      BLECharacteristic::PROPERTY_NOTIFY);
-  pTxCharacteristic->addDescriptor(new BLE2902());
-
-  BLECharacteristic *pRxCharacteristic = pService->createCharacteristic(
-      CHARACTERISTIC_UUID_RX,
-      BLECharacteristic::PROPERTY_WRITE);
-  pRxCharacteristic->setCallbacks(new BitBLECharacteristic());
-
-  pService->start();
-  pServer->getAdvertising()->start();
-
-  xTaskCreate(notifierTask, "notifierTask", 4096, NULL, 1, NULL);
-  xTaskCreate(WiFiWorker::TaskHandler, "WiFiWorker::TaskHandler", 8192, flags, 1, NULL);
+  xTaskCreate(BLEWorker::TaskHandler, "BLEWorker::TaskHandler", 8192, flags, 1, nullptr);
+  xTaskCreate(WiFiWorker::TaskHandler, "WiFiWorker::TaskHandler", 8192, flags, 1, nullptr);
 
   Serial.println("Waiting a client connection to notify...");
 }
 
-void loop()
-{
+void loop() {
   vTaskDelete(nullptr);
 }
