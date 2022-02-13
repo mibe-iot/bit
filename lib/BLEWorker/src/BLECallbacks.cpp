864d19e393ccfc8ef67b89707253ed555ea70739
Maksim Yersh <edelwud@icloud.com>
2022-02-13 23:30:18 +0300

    update: ble states

--- lib/BLEWorker/src/BLECallbacks.cpp
+++ lib/BLEWorker/src/BLECallbacks.cpp
@@ -1,16 +1,19 @@
+#include <Arduino.h>
 #include <BLECallbacks.h>
 
 BLECallbacks::BLECallbacks(EventGroupHandle_t flags) {
-    xEventGroupClearBits(flags, BLEConnected);
+    xEventGroupClearBits(flags, BLEState::CONNECTED);
     this->flags = flags;
 }
 
 void BLECallbacks::onConnect(BLEServer *pServer) {
-    xEventGroupSetBits(flags, BLEConnected);
+    xEventGroupSetBits(flags, BLEState::CONNECTED);
+    Serial.println("Thinker connected");
     BLEServerCallbacks::onConnect(pServer);
 }
 
 void BLECallbacks::onDisconnect(BLEServer *pServer) {
-    xEventGroupClearBits(flags, BLEConnected);
+    xEventGroupClearBits(flags, BLEState::CONNECTED);
+    Serial.println("Thinker disconnected");
     BLEServerCallbacks::onDisconnect(pServer);
 }
