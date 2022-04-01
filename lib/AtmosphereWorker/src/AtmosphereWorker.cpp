86b39c3a3433e52cc94d08148753294c853820aa
Maksim Yersh <edelwud@icloud.com>
2022-04-02 02:07:39 +0300

    feat(atmosphere): json pack & mqtt publish

--- lib/AtmosphereWorker/src/AtmosphereWorker.cpp
+++ lib/AtmosphereWorker/src/AtmosphereWorker.cpp
@@ -1,6 +1,9 @@
 #include <AtmosphereWorker.h>
 #include <SharedState.h>
+#include <ArduinoJson.h>
 #include <DHTesp.h>
+
+const char TAG[] = "DHT";
 
 void AtmosphereWorker::TaskHandler(void *param) {
     auto state = (SharedState *) param;
@@ -8,16 +11,26 @@ void AtmosphereWorker::TaskHandler(void *param) {
     dht.setup(15, DHTesp::DHT11);
 
     while (true) {
-        auto result = xEventGroupWaitBits(state->flags, SharedConnectivityState::WIFI_CONNECTED, pdFALSE, pdTRUE,
+        auto result = xEventGroupWaitBits(state->flags, SharedConnectivityState::MQTT_CONNECT, pdFALSE, pdTRUE,
                                           pdMS_TO_TICKS(100));
-        if (!(result & SharedConnectivityState::WIFI_CONNECTED)) {
+        if (!(result & SharedConnectivityState::MQTT_CONNECT)) {
             vTaskDelay(pdMS_TO_TICKS(300));
             continue;
         }
 
-        float temperature = dht.getTemperature();
-        float humidity = dht.getHumidity();
-        Serial.printf("temperature: %f, humidity: %f\n", temperature, humidity);
+        DynamicJsonDocument document(1024);
+        document[F("reportType")] = "atmosphere";
+        document[F("reportData")]["temperature"] = dht.getTemperature();
+        document[F("reportData")]["humidity"] = dht.getHumidity();
+
+        char buf[1024];
+        serializeJson(document, buf, 1024);
+
+        auto endpoint = "/mibe/reports/" + state->configuration->mqtt->GetDeviceIdentifier();
+        if (!state->mqtt->publish(endpoint.c_str(), buf)) {
+            ESP_LOGE(TAG, "cannot to report atmosphere data");
+        }
+
         vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
