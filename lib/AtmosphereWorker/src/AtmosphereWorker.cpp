8cfacec6a3da68b3652429f1df326f6357250cb7
Maksim Yersh <edelwud@icloud.com>
2022-04-01 23:49:28 +0300

    feat: add `AtmosphereWorker` module

---
+++ lib/AtmosphereWorker/src/AtmosphereWorker.cpp
@@ -0,0 +1,23 @@
+#include <AtmosphereWorker.h>
+#include <SharedState.h>
+#include <DHTesp.h>
+
+void AtmosphereWorker::TaskHandler(void *param) {
+    auto state = (SharedState *) param;
+    DHTesp dht;
+    dht.setup(15, DHTesp::DHT11);
+
+    while (true) {
+        auto result = xEventGroupWaitBits(state->flags, SharedConnectivityState::WIFI_CONNECTED, pdFALSE, pdTRUE,
+                                          pdMS_TO_TICKS(20));
+        if (!(result & SharedConnectivityState::WIFI_CONNECTED)) {
+            vTaskDelay(pdMS_TO_TICKS(300));
+            continue;
+        }
+
+        float temperature = dht.getTemperature();
+        float humidity = dht.getHumidity();
+        Serial.printf("temperature: %f, humidity: %f\n", temperature, humidity);
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
+}
