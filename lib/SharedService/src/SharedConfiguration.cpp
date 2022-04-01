77c97ee92378a88fc6f384d4d137d75364b21a8f
Maksim Yersh <edelwud@icloud.com>
2022-04-02 02:04:55 +0300

    feat(state): assign mqtt conf to shared state

--- lib/SharedService/src/SharedConfiguration.cpp
+++ lib/SharedService/src/SharedConfiguration.cpp
@@ -3,9 +3,11 @@
 SharedConfiguration::SharedConfiguration() {
     ble = new BleConfiguration();
     wifi = new WifiConfiguration();
+    mqtt = new MQTTConfiguration();
 }
 
 SharedConfiguration::~SharedConfiguration() {
     delete ble;
     delete wifi;
+    delete mqtt;
 }
